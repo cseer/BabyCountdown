b8e8490c8109b4c0f90d476edfcc0b77bbe8ece6
Carlos <carlos.seer@gmail.com>
2020-08-29 01:24:18 -0400

    Initial commit

---
+++ babycountdown/src/main/java/countdown/babycountdown/App.java
@@ -0,0 +1,245 @@
+package countdown.babycountdown;
+
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+
+
+import org.ini4j.Wini;
+
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.io.File;
+import javax.swing.JButton;
+import javax.swing.ImageIcon;
+import java.awt.event.ActionListener;
+import javax.swing.JToolBar;
+
+public class App {
+
+
+	public String getPhrase() {
+		return strPhrase;
+	}
+
+	public void setPhrase(String phrase) {
+		this.strPhrase = phrase;
+	}
+
+	public LocalDate getDueDate() {
+		return dueDate;
+	}
+
+	public void setDueDate(LocalDate dueDate) {
+		this.dueDate = dueDate;
+	}
+
+	private JFrame frmBabyCountdown;
+	String strPhrase = "for Baby to arrive";
+	LocalDate dueDate = LocalDate.now().plusMonths(9);
+	String strWeeks =  "0 weeks";
+	String strDays = "0 days";	
+	boolean showDueDate = true;
+	String strDueDate = "";
+	
+	
+	static boolean configOK = false;
+	
+ 	private void readIni() {
+		//DEBUG
+		System.out.println("Ejecutando readIni");
+		try {
+			File f = new File("countdown.ini");
+			if (!f.exists()) {
+				f = null;
+				System.out.println("ERROR in readIni: countdown.ini do not exist");
+				displayConfigDialog(true);
+				//TODO Validar si se creo exitosament archivo INI
+		   	} 
+			// [Por ahora] asumo que si no existia, se creo exitosamente archivo INI
+			f = new File("countdown.ini");
+			
+			//Parse values form INI file
+			Wini ini = new Wini(f);
+
+            
+            String sDueDate = ini.get("config", "duedate");     
+            try {
+            	dueDate = LocalDate.parse(sDueDate);
+            } catch (Exception e) {
+            	System.out.println(e.getMessage());
+            	dueDate = LocalDate.now().plusMonths(9);
+            	ini.put("config", "duedate", dueDate.toString());
+            	ini.store();
+			}
+
+			
+			// Calculate period and update value of strWeeks and strDays
+			LocalDate now = LocalDate.now();
+			//LocalDate now = LocalDate.of(2020,8,22);
+			long lWeeks = ChronoUnit.WEEKS.between(now, dueDate);
+			long lDays = ChronoUnit.DAYS.between(now, dueDate);
+			int days = 0;
+			if (lWeeks > 0) {
+				days = (int) (lDays % lWeeks);
+			}
+			//String dueDate = "4/6/2021";
+			strWeeks =  lWeeks + " weeks";
+			strDays = days + " days";	
+			
+            strPhrase= ini.get("config", "phrase");
+            if (strPhrase == null) {
+            	strPhrase = "for Baby to arrive";
+            	ini.put("config", "phrase", strPhrase);
+            	ini.store();
+            }
+			
+			String sShowDueDate = ini.get("config", "showduedate");   
+			if (sShowDueDate==null) {
+				sShowDueDate = "true";
+            	ini.put("config", "showduedate", sShowDueDate);
+            	ini.store();
+			}
+			if (sShowDueDate.trim().equals("true")) {
+				showDueDate = true;
+				strDueDate = "DUE DATE: " + dueDate.getMonth() + " " +  dueDate.getDayOfMonth() + ", " + dueDate.getYear(); 
+			} else {
+				showDueDate = false;
+				strDueDate = ""; 				
+			}
+			
+			//TODO
+			System.out.println(sDueDate);		   	
+		}catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
+	}
+	
+	private void displayConfigDialog(boolean tForceSave) {
+		ConfDialog confDialog = new ConfDialog();	
+		confDialog.setDueDate(dueDate.toString());
+		confDialog.setPrase(strPhrase);	
+		confDialog.setShowDueDate(showDueDate);
+		confDialog.setForceSave(tForceSave);
+		confDialog.setVisible(true);
+		//TODO
+		System.out.println("Despues de cerrar ConfigDialog");
+		
+	}
+	
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				//leer ini file						
+				try {
+					App window = new App();
+					window.frmBabyCountdown.setVisible(true);								
+				} catch (Exception e) {
+					e.printStackTrace();
+				}								
+
+			}
+		});
+	}
+	
+//	private static Logger logger = LogManager.getLogger(Countdown.class);
+
+	/**
+	 * Create the application.
+	 */
+	public App() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		readIni();
+		//logger.info("Starting");
+		frmBabyCountdown = new JFrame();
+		frmBabyCountdown.setResizable(false);
+		frmBabyCountdown.setIconImage(Toolkit.getDefaultToolkit().getImage(App.class.getResource("/resources/012_026_newborn_infant_child_baby-256.png")));
+		frmBabyCountdown.setTitle("Baby Countdown");
+		frmBabyCountdown.getContentPane().setFont(new Font("SansSerif", Font.PLAIN, 12));
+		frmBabyCountdown.setBounds(100, 100, 410, 305);
+		frmBabyCountdown.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmBabyCountdown.getContentPane().setLayout(null);
+		
+		JLabel lblTitle = new JLabel("BABY COUNTDOWN");		
+		lblTitle.setFont(new Font("SansSerif", Font.PLAIN, 34));
+		lblTitle.setHorizontalAlignment(SwingConstants.CENTER);
+		lblTitle.setBounds(6, 37, 404, 52);
+		frmBabyCountdown.getContentPane().add(lblTitle);		
+		
+		final JLabel lblWeeks = new JLabel(strWeeks);
+		lblWeeks.setHorizontalAlignment(SwingConstants.CENTER);
+		lblWeeks.setFont(new Font("SansSerif", Font.PLAIN, 29));
+		lblWeeks.setBounds(100, 102, 193, 38);
+		frmBabyCountdown.getContentPane().add(lblWeeks);
+		
+		
+		final JLabel lblDays = new JLabel(strDays);
+		lblDays.setHorizontalAlignment(SwingConstants.CENTER);
+		lblDays.setFont(new Font("SansSerif", Font.PLAIN, 29));
+		lblDays.setBounds(100, 141, 193, 38);
+		frmBabyCountdown.getContentPane().add(lblDays);
+		
+		final JLabel lblPhrase = new JLabel(strPhrase);
+		lblPhrase.setHorizontalAlignment(SwingConstants.CENTER);
+		lblPhrase.setFont(new Font("SansSerif", Font.PLAIN, 20));
+		lblPhrase.setBounds(67, 191, 282, 38);
+		frmBabyCountdown.getContentPane().add(lblPhrase);
+		
+		
+		final JLabel lblDueDate = new JLabel(strDueDate);
+		lblDueDate.setHorizontalAlignment(SwingConstants.CENTER);
+		lblDueDate.setFont(new Font("SansSerif", Font.ITALIC, 12));
+		lblDueDate.setBounds(67, 241, 282, 15);
+		frmBabyCountdown.getContentPane().add(lblDueDate);
+		
+		JToolBar toolBar = new JToolBar();
+		toolBar.setFloatable(false);
+		toolBar.setBounds(6, 0, 398, 35);
+		frmBabyCountdown.getContentPane().add(toolBar);
+		
+		JButton btnSetup = new JButton("");
+		btnSetup.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				displayConfigDialog(false);
+				readIni();	
+				lblPhrase.setText(strPhrase);
+				lblWeeks.setText(strWeeks);
+				lblDays.setText(strDays);
+				lblDueDate.setText(strDueDate);
+				frmBabyCountdown.revalidate();
+				frmBabyCountdown.repaint();		
+			}
+		});
+		btnSetup.setIcon(new ImageIcon(App.class.getResource("/SmallIcons/Settings24.png")));
+		toolBar.add(btnSetup);
+		
+		JButton btnNewButton = new JButton("");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				AboutProgram about = new AboutProgram ();				
+				about.setVisible(true);
+			}
+		});
+		btnNewButton.setIcon(new ImageIcon(App.class.getResource("/SmallIcons/Information24.png")));
+		toolBar.add(btnNewButton);
+
+		
+
+	}
+}
---
+++ babycountdown/src/main/java/countdown/babycountdown/ConfDialog.java
@@ -0,0 +1,280 @@
+package countdown.babycountdown;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import org.ini4j.Wini;
+
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.time.LocalDate;
+import java.awt.event.ActionEvent;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+import java.awt.Font;
+import java.awt.Toolkit;
+
+import javax.swing.ImageIcon;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.SwingConstants;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+@SuppressWarnings("serial")
+public class ConfDialog extends JDialog {
+
+	public String getPhrase() {
+		return tPhrase;
+	}
+
+	public void setPrase(String countDesc) {
+		txtPhrase.setText(countDesc); 		
+		this.tPhrase = countDesc;
+	}
+
+	
+	public String getDueDate() {
+		return tDueDate;
+	}
+
+	public void setDueDate(String sDueDate) {	
+		//txtDueDate.setText(sDueDate);
+		LocalDate dueDate = LocalDate.parse(sDueDate);
+		txtYear.setText(Integer.toString(dueDate.getYear())) ;
+		txtDay.setText(Integer.toString(dueDate.getDayOfMonth()));
+		int iMonth= dueDate.getMonthValue() - 1;
+		comboBox.setSelectedIndex(iMonth);
+		this.tDueDate = sDueDate;
+	}
+
+
+
+	public boolean isShowDueDate() {
+		return showDueDate;
+	}
+
+	public void setShowDueDate(boolean showDueDate) {
+		ckbxShowDueDate.setSelected(showDueDate);
+		this.showDueDate = showDueDate;		
+	}
+
+	public boolean isForceSave() {
+		return forceSave;
+	}
+
+	public void setForceSave(boolean forceSave) {
+		this.forceSave = forceSave;
+	}
+
+
+	String tPhrase = ""; 
+	String tDueDate = "";
+	boolean showDueDate = false;
+	boolean forceSave = false;
+	
+	private final JPanel contentPanel = new JPanel();
+	private JTextField txtPhrase;
+	private JCheckBox ckbxShowDueDate;
+	@SuppressWarnings("rawtypes")
+	private JComboBox comboBox;
+	private JTextField txtDay;
+	String[] months = {" January", " February", " March", " April", " May", " June", 
+					   " July", " August" , " September", " October", " November", " December"};
+	private JTextField txtYear;
+
+	private String buildDueDateString() {
+    	int iMonth = comboBox.getSelectedIndex()+1;
+    	String sMonth = Integer.toString(iMonth);
+    	if (iMonth < 10) {
+    		sMonth = "0"+sMonth;
+    	}
+    	String sDay = txtDay.getText();
+    	if ( Integer.valueOf(sDay) < 10) {
+    		sDay = "0"+sDay;
+    	}
+    	
+    	String sDueDate = txtYear.getText() + "-" + sMonth + "-" + sDay ;  
+    	return sDueDate;
+	}
+	
+	private boolean isDueDateValid() {
+		String sDueDate = buildDueDateString();
+		boolean validDate = true;
+		try {
+			@SuppressWarnings("unused")
+			LocalDate tDueDate = LocalDate.parse(sDueDate);
+		}catch(Exception e) {
+			validDate = false;
+		}	
+		return validDate;
+	}
+	
+	void saveIni() {
+		try{
+			File f = new File("countdown.ini");
+			if (!f.exists()) {
+				System.out.println("ERROR in ConfDialog: countdown.ini do not exist");
+				System.out.println("Creating the file");
+				f.createNewFile();
+		   	}
+        	Wini ini = new Wini(f);
+        	
+        	String sDueDate = LocalDate.now().plusMonths(9).toString();
+        	if (isDueDateValid()) {
+        		sDueDate = buildDueDateString() ;   
+        	} 
+        	
+        	ini.put("config", "duedate", sDueDate);
+        	ini.put("config", "phrase", txtPhrase.getText());
+        	if (showDueDate) {
+        		ini.put("config", "showduedate", "true");
+        	} else {
+        		ini.put("config", "showduedate", "false");
+        	};
+            ini.store();
+            f = null;
+        // To catch basically any error related to writing to the file
+        // (The system cannot find the file specified)
+        }catch(Exception e){
+            System.err.println(e.getStackTrace());
+        }
+	}
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			ConfDialog dialog = new ConfDialog();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public ConfDialog() {
+		addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				if (forceSave) {
+					saveIni();				
+				} 				
+			}
+		});
+		setResizable(false);
+		setIconImage(Toolkit.getDefaultToolkit().getImage(ConfDialog.class.getResource("/SmallIcons/Settings24.png")));
+		setFont(new Font("DejaVu Serif", Font.PLAIN, 12));
+		setTitle("Baby's Information");
+		setModal(true);
+		setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+		setAlwaysOnTop(true);
+		setBounds(100, 100, 450, 246);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		
+		JLabel lblPhrase = new JLabel("Phrase:");
+		lblPhrase.setBounds(54, 83, 60, 15);
+		contentPanel.add(lblPhrase);	
+	
+		txtPhrase = new JTextField();
+		txtPhrase.setBounds(165, 77, 222, 27);
+		txtPhrase.setColumns(10);
+		txtPhrase.setText(tPhrase); 				
+		contentPanel.add(txtPhrase);
+
+		
+		JLabel lblDueDate = new JLabel("Due Date:");
+		lblDueDate.setBounds(54, 44, 99, 15);		
+		contentPanel.add(lblDueDate);
+		
+		ckbxShowDueDate = new JCheckBox("Show Due Date");
+		ckbxShowDueDate.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				showDueDate = ckbxShowDueDate.isSelected();				
+			}
+		});
+		ckbxShowDueDate.setBounds(54, 122, 200, 23);
+		contentPanel.add(ckbxShowDueDate);
+		
+		txtDay = new JTextField();
+		txtDay.setFont(new Font("SansSerif", Font.PLAIN, 12));
+		txtDay.setHorizontalAlignment(SwingConstants.CENTER);
+		txtDay.setText("1");
+		txtDay.setBounds(277, 42, 27, 23);
+		contentPanel.add(txtDay);
+		txtDay.setColumns(10);
+		
+		comboBox = new JComboBox(months);
+		comboBox.setFont(new Font("SansSerif", Font.PLAIN, 12));
+		comboBox.setBounds(165, 41, 101, 24);
+		contentPanel.add(comboBox);
+		
+		txtYear = new JTextField();
+		txtYear.setFont(new Font("SansSerif", Font.PLAIN, 12));
+		txtYear.setBounds(316, 42, 71, 23);
+		contentPanel.add(txtYear);
+		txtYear.setColumns(10);
+
+			
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setFont(new Font("SansSerif", Font.PLAIN, 12));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			buttonPane.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+			{
+				JButton btnCancel = new JButton("Cancel");
+				btnCancel.setIcon(new ImageIcon(ConfDialog.class.getResource("/SmallIcons/Cancel.png")));
+				btnCancel.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent arg0) {										
+						if (forceSave) {
+							saveIni();
+						} 
+						dispose();
+						
+					}
+				});
+				btnCancel.setActionCommand("Cancel");
+				buttonPane.add(btnCancel);
+			}
+			{
+				JButton btnOk = new JButton("OK");
+				btnOk.setIcon(new ImageIcon(ConfDialog.class.getResource("/SmallIcons/Check.png")));
+				btnOk.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent arg0) {
+						System.out.println("Click Ok (Save)");
+						if (isDueDateValid()) {
+							saveIni();
+							dispose();
+						} else {
+							JOptionPane.showMessageDialog(
+									null,
+								    "Invalid Date",
+								    "ERROR",
+								    JOptionPane.ERROR_MESSAGE);
+
+						}
+							
+					}
+				});
+				btnOk.setActionCommand("Ok");
+				buttonPane.add(btnOk);
+				getRootPane().setDefaultButton(btnOk);
+			}
+		}
+	}
+}
